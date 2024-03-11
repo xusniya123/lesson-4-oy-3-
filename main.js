5fd5d3b4fae4530f5b82733eccee5ee4827abf27
xusniya123 <xusniya12345678@gmail.com>
2024-03-11 13:43:00 +0500

    Lesson 4 moth (3)

---
+++ main.js
@@ -0,0 +1,23 @@
+// let numburFirst = Number(prompt("enter first number ="));
+//  let numburSecond = Number(prompt("enter second number ="));
+
+//  let sum, a, b, c;
+
+//  sum = numburFirst + numburSecond;
+//  a = numburFirst - numburSecond;
+//  b = numburFirst / numburSecond;
+//  c = Math.pow(numburFirst, numburSecond);
+
+
+
+
+
+//  console.log(sum);
+//  console.log(a);
+//  console.log(b);
+// console.log(c);
+
+let a = Math.random();
+let b = a * 2;
+let c = a % 2;
+alert("random =" + a + "\n random * 2 =" + b + "\n random % 2 " + c );
